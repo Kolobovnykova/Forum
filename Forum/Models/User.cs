0443bbd297463c5a8c5ee72645db76002fbb7e8e
Anastasiia <nastena31011993@gmail.com>
2018-07-06 23:15:13 +0300

    Added user model, views and controller

---
+++ Forum/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+       // [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Avatar { get; set; }
+        [Required]
+        public string Email { get; set; }
+        //public List<PostDTO> Posts { get; set; }
+        //public List<TodoDTO> Todos { get; set; }
+        //public List<CommentDTO> Comments { get; set; }
+    }
+}
---
+++ Forum/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Models;
+
+namespace Forum.Services
+{
+    public class UserService
+    {
+        private static List<User> users;
+
+        public UserService()
+        {
+            if (users == null)
+            {
+                users = new List<User>
+                {
+                    new User {Avatar = "", CreatedAt = DateTime.Now, Email = "123", Id = 1, Name = "Name1"},
+                    new User {Avatar = "", CreatedAt = DateTime.Now, Email = "123", Id = 2, Name = "Name2"},
+                    new User {Avatar = "", CreatedAt = DateTime.Now, Email = "444", Id = 3, Name = "Name3"},
+                    new User {Avatar = "", CreatedAt = DateTime.Now, Email = "532", Id = 4, Name = "Name4"},
+                    new User {Avatar = "", CreatedAt = DateTime.Now, Email = "123", Id = 5, Name = "Name5"}
+                };
+            }
+        }
+
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        public void AddUser(User model)
+        {
+            model.CreatedAt = DateTime.Now;
+            users.Add(model);
+        }
+
+        public void DeleteById(int id)
+        {
+            users.Remove(users.FirstOrDefault(x => x.Id == id));
+        }
+
+        public User GetById(int id)
+        {
+            return users.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
